700f9494b58ece2264cae27a3c351b71dfda811a
MeowyMacMeowza <50887758+MeowyMacMeowza@users.noreply.github.com>
2026-08-20 01:35:26 +0800

    Add doneness to Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,26 @@
 public class Task {
+    private boolean isDone;
     private String name;
 
     public Task(String name) {
         this.name = name;
+        this.isDone = false;
+    }
+
+    public boolean doTask() {
+        if(isDone) return false;
+        isDone = true;
+        return true;
+    }
+
+    public boolean undoTask() {
+        if(!isDone) return false;
+        isDone = false;
+        return true;
     }
 
     @Override
     public String toString() {
-        return name;
+        return String.format("[%s] %s",isDone ? "x" : " ",name);
     }
 }
