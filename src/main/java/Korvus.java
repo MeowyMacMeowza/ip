0a1376d7df61bdb90025a6064e3c3ed945534123
MeowyMacMeowza <50887758+MeowyMacMeowza@users.noreply.github.com>
2026-08-19 21:57:18 +0800

    Add echoing for Korvus
    Made it so that Korvus repeats what the user types.
    Added a say function to dynamically format output.

--- src/main/java/Korvus.java
+++ src/main/java/Korvus.java
@@ -1,5 +1,9 @@
-public class Korvus {
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 
+public class Korvus {
+    private static int MAX_LENGTH = 60;
     private static String banner =
             """
              ,_
@@ -9,28 +13,91 @@ public class Korvus {
              |_|\\_|____|_|  \\_/  \\__,_|\\___/
             """;
 
+    private Scanner userInput;
+    private PrintStream botOutput;
+
+    private Korvus(InputStream input, PrintStream output) {
+        this.userInput = new Scanner(input);
+        this.botOutput = output;
+    }
+
     public static void main(String[] args) {
-        greeting();
-        goodbye();
+        Korvus bot = new Korvus(System.in, System.out);
+
+        bot.greet();
+        bot.start();
     }
 
-    private static void greeting() {
+    private void start() {
+        while(true) {
+            String userReply = userInput.nextLine();
+
+            switch (userReply) {
+                case "bye":
+                    goodbye();
+                default:
+                    say(userReply);
+                    divider();
+            }
+        }
+    }
+
+    private void greet() {
         divider();
-        System.out.println(banner);
-        System.out.println("> Nice to meet you!\n> I am caw-lled Korvus, your personal chatbot for keeping\n  track of shiny things.");
+        botOutput.println(banner);
+        say("Nice to meet you!");
+        say("I am caw-lled Korvus, your personal chatbot for keeping track of shiny things.");
         divider();
     }
 
-    private static void divider() {
-        System.out.println("____________________________________________________________");
+    private void divider() {
+        StringBuilder divider = new StringBuilder();
+        divider.repeat("_", MAX_LENGTH);
+
+        botOutput.println(divider);
     }
 
-    private static void goodbye() {
-        System.out.println("> Goodbye! Eagle to see you again!");
+    private void goodbye() {
+        say("Goodbye! Eagle to see you again!");
         divider();
 
-        // For now, redundant code
+        // Only runs when user says bye
         System.exit(0);
     }
-}
 
+    // For formatting
+    private void say(String text) {
+        int lastSpace = -1, lastLine = -1;
+        StringBuilder newText = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            if(text.charAt(i) == ' ') {
+                lastSpace = i;
+            }
+
+            //End of line
+            if(i - lastLine > MAX_LENGTH - 2) {
+                if(newText.isEmpty()) newText.append("> ");
+                else newText.append("  ");
+
+                if(lastSpace > lastLine) {
+                    newText.append(text.substring(lastLine + 1, lastSpace));
+                    newText.append("\n");
+
+                    lastLine = lastSpace;
+                } else {
+                    newText.append(text.substring(lastLine + 1, lastLine + MAX_LENGTH - 1));
+                    newText.append("\n");
+
+                    lastLine += MAX_LENGTH - 2;
+                }
+            }
+        }
+
+        if(newText.isEmpty()) newText.append("> ");
+        else newText.append("  ");
+        newText.append(text.substring(lastLine + 1));
+
+        botOutput.println(newText);
+    }
+}
