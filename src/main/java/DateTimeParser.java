e03e4d248d778306484043081a00d2b860afe440
MeowyMacMeowza <50887758+MeowyMacMeowza@users.noreply.github.com>
2026-08-28 02:38:24 +0800

    Add DateTimeParser to error check date inputs

---
+++ src/main/java/DateTimeParser.java
@@ -0,0 +1,28 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateTimeParser {
+    private DateTimeFormatter formatter;
+
+    public DateTimeParser (DateTimeFormatter formatter) {
+        this.formatter = formatter;
+    }
+
+    public boolean isValid(String sDatetime) {
+        try{
+            formatter.parse(sDatetime);
+        } catch (DateTimeParseException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public LocalDateTime parseDateString(String sDatetime) {
+        try{
+            return LocalDateTime.parse(sDatetime, formatter);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+}
