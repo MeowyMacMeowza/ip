fed83acc6083b269a9e5b0a6e120ce3692318b6b
MeowyMacMeowza <50887758+MeowyMacMeowza@users.noreply.github.com>
2026-08-28 15:07:32 +0800

    Add some JUnit tests for Task.java

--- src/test/java/korvus/tasks/TaskTest.java
+++ src/test/java/korvus/tasks/TaskTest.java
@@ -1,4 +1,33 @@
 package korvus.tasks;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TaskTest {
+
+    @Test
+    public void taskCreation_noError(){
+        assertDoesNotThrow(() -> new Task("Sample Task", null));
+        assertDoesNotThrow(() -> new Task("Sample Task 2", null));
+        assertDoesNotThrow(() -> new Task("", null));
+    }
+
+    @Test
+    public void taskCreation_nullError(){
+        assertThrows(NullPointerException.class, () -> new Task(null, null));
+    }
+
+    @Test
+    public void taskCreation_invalidTaskError(){
+        assertThrows(InvalidTaskException.class, () -> new Task("-invalid task name", null));
+        assertThrows(InvalidTaskException.class, () -> new Task("-", null));
+    }
+
+    @Test
+    public void taskGeneration_invalidTaskError(){
+        assertThrows(InvalidTaskException.class, () -> Task.generateTask("-d -invalid name // some date", null));
+        assertThrows(InvalidTaskException.class, () -> Task.generateTask("-t -still bad", null));
+        assertThrows(InvalidTaskException.class, () -> Task.generateTask("-e -", null));
+    }
 }
