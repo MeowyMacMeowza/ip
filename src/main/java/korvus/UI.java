76905e15e3806c3f672122deff20715b7191ed1c
MeowyMacMeowza <50887758+MeowyMacMeowza@users.noreply.github.com>
2026-08-31 02:20:55 +0800

    Add Javadocs to UI.java

--- src/main/java/korvus/UI.java
+++ src/main/java/korvus/UI.java
@@ -9,30 +9,65 @@ public class UI {
     private PrintStream outputStream;
     private int maxLength;
 
+    /**
+     * Returns an instance of UI.
+     *
+     * @param inStream Input stream that users will input from.
+     * @param outStream Output stream that the UI should print to.
+     * @param maxLength Max length of characters in the outputStream.
+     */
     public UI(InputStream inStream, PrintStream outStream, int maxLength) {
         this.inputScanner = new Scanner(inStream);
         this.outputStream = outStream;
         this.maxLength = maxLength;
     }
 
+    /**
+     * Returns an instance of UI.
+     *
+     * @param inStream Input stream that users will input from.
+     * @param outStream Output stream that the UI should print to.
+     */
     public UI(InputStream inStream, PrintStream outStream) {
         this(inStream, outStream, 70);
     }
 
+    /**
+     * Returns the user's input. The program pauses until a new input is detected.
+     *
+     * @return User's input into the input stream.
+     */
     public String listen() {
         return inputScanner.nextLine().trim();
     }
 
+    /**
+     * Sets the max length of the output.
+     *
+     * @param length New length to be updated
+     * @return The previous max length used.
+     */
     public int setMaxLength(int length) {
         int oldLength = this.maxLength;
         this.maxLength = length;
         return oldLength;
     }
 
+    /**
+     * Returns the max length of the output.
+     *
+     * @return The max length of the output.
+     */
     public int getMaxLength() {
         return this.maxLength;
     }
 
+    /**
+     * Sends the provided message into the output stream.
+     * Custom formatting is used to make the output more readable.
+     *
+     * @param msg Message to be outputted.
+     */
     public void say(String msg) {
         if(msg.isEmpty()) {
             System.out.println("> Caw");
@@ -73,6 +108,11 @@ public class UI {
         outputStream.println(newText);
     }
 
+    /**
+     * Prints the provided message into the output stream with no formatting
+     *
+     * @param s Message to be outputted.
+     */
     public void rawPrint(String s) {
         outputStream.println(s);
     }
