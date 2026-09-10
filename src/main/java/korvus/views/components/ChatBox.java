8d4b095c6cb0d4767e53535d56d6d5b1424d0db1
MeowyMacMeowza <50887758+MeowyMacMeowza@users.noreply.github.com>
2026-09-11 03:34:59 +0800

    Refactor ChatBox to improve readability

--- src/main/java/korvus/views/components/ChatBox.java
+++ src/main/java/korvus/views/components/ChatBox.java
@@ -3,18 +3,36 @@ package korvus.views.components;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 
+/**
+ * ChatBox class for to contain the chat messages for the GUI.
+ */
 public class ChatBox extends ScrollPane {
     private VBox chatLog;
 
+    /**
+     * Returns a ChatBox to be displayed in the App GUI.
+     */
     public ChatBox() {
         this.chatLog = new VBox();
-        this.setContent(chatLog);
 
+        this.setContent(chatLog);
+        this.setUpScrollPane();
+    }
+
+    /**
+     * Initialises the ChatBox to the correct parameters.
+     */
+    private void setUpScrollPane() {
         this.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         this.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
         this.vvalueProperty().bind(chatLog.heightProperty());
     }
 
+    /**
+     * Adds a new ChatMessage to the ChatBox.
+     *
+     * @param chatMessage ChatMessage to be added.
+     */
     public void addMessage(ChatMessage chatMessage) {
         this.chatLog.getChildren().add(chatMessage);
 
