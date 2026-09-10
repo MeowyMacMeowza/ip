ca9387c711c2859e0024d061f21e278778daee70
MeowyMacMeowza <50887758+MeowyMacMeowza@users.noreply.github.com>
2026-09-11 07:38:17 +0800

    Refactor Launcher to improve readability

--- src/main/java/korvus/Launcher.java
+++ src/main/java/korvus/Launcher.java
@@ -4,11 +4,22 @@ import javafx.application.Application;
 import korvus.bot.CommandLineKorvus;
 import korvus.ui.AppUI;
 
+/**
+ * Main Class that gets ran.
+ */
 public class Launcher {
     /**
      * Creates the Korvus bot and starts it.
      */
     public static void main(String... args) {
+        if (isCli(args)) {
+            new CommandLineKorvus(System.in, System.out, "data/").initialise();
+        } else {
+            Application.launch(AppUI.class, args);
+        }
+    }
+
+    private static boolean isCli(String... args) {
         boolean isCli = false;
 
         for (String arg : args) {
@@ -17,10 +28,6 @@ public class Launcher {
             }
         }
 
-        if (isCli) {
-            new CommandLineKorvus(System.in, System.out, "data/").initialise();
-        } else {
-            Application.launch(AppUI.class, args);
-        }
+        return isCli;
     }
 }
