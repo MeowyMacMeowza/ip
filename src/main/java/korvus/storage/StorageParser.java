21420bc2f4d41b0eb35dca082fd79e4cb6180bdc
MeowyMacMeowza <50887758+MeowyMacMeowza@users.noreply.github.com>
2026-09-11 02:36:31 +0800

    Refactor StorageParser to standardise naming

--- src/main/java/korvus/storage/StorageParser.java
+++ src/main/java/korvus/storage/StorageParser.java
@@ -11,18 +11,18 @@ import java.io.IOException;
  */
 public class StorageParser<T extends Storable<T>> {
     private Storage storage;
-    private String filePath;
+    private String fileName;
 
     /**
      * Returns an instance of the object StorageParser.
      *
      * @param storage Storage object pointing to the directory with files.
-     * @param filePath (Relative) path of the file to be read.
+     * @param fileName Name of the file to be accessed.
      * @throws StorageConflictException If another parser in storage is pointing to the same file.
      */
-    public StorageParser(Storage storage, String filePath) throws StorageConflictException {
+    public StorageParser(Storage storage, String fileName) throws StorageConflictException {
         this.storage = storage;
-        this.filePath = filePath;
+        this.fileName = fileName;
 
         storage.addParser(this);
     }
@@ -34,7 +34,7 @@ public class StorageParser<T extends Storable<T>> {
      * @return Boolean whether the current parser conflicts with the given parser.
      */
     public <S extends Storable<S>> boolean isParserConflict(StorageParser<S> parser) {
-        return this.filePath.equals(parser.filePath);
+        return this.fileName.equals(parser.fileName);
     }
 
     /**
@@ -44,9 +44,10 @@ public class StorageParser<T extends Storable<T>> {
      * @throws IOException If there are any errors in reading the file.
      */
     public String readStorage() throws IOException {
-        BufferedReader reader = storage.readFile(filePath);
+        BufferedReader reader = storage.readFile(fileName);
         String taskslistString = reader.readAllAsString();
         reader.close();
+
         return taskslistString;
     }
 
@@ -57,7 +58,7 @@ public class StorageParser<T extends Storable<T>> {
      * @throws IOException If there are any errors in writing to the file.
      */
     public void writeStorage(T t) throws IOException {
-        BufferedWriter writer = storage.writeFile(filePath);
+        BufferedWriter writer = storage.writeFile(fileName);
         writer.write(t.writeToString());
         writer.close();
     }
